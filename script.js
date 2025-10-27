e3aac097adff3d50505a98adcd82451ed44c4453
sxmurxi35 <sxmurxin@proton.me>
2025-10-27 18:46:21 +0100

    Added function for displaying library

--- script.js
+++ script.js
@@ -10,7 +10,11 @@ function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = read
+    if(read == true){
+        this.read = 'Read'
+    } else {
+        this.read = 'Not read'
+    }
     this.id = crypto.randomUUID()
 }
 
@@ -19,3 +23,21 @@ function addBookToLibrary(title, author, pages, read) {
 
     library.push(book)
 }
+
+function displayLibrary() {
+    const main = document.querySelector('#main')
+
+    for(const book of library){
+        const bookSection = document.createElement('section')
+        bookSection.innerHTML = `
+        <h3 id = 'book-title'>${book.title}</h3>
+        <p class = 'book-info'>${book.author}</p>
+        <p class = 'book-info'>${book.pages}</p>
+        <p class = 'book-info'>${book.read}</p>
+        `
+
+        main.appendChild(bookSection)
+    }
+}
+
+displayLibrary()
